d3cc51d3adc5a980cd2e2af3144aeca2cff93477
BraianRodriguez21 <braianrodriguezomg@gmail.com>
2024-04-16 15:52:57 -0300

    preeentrega

--- src/app.js
+++ src/app.js
@@ -9,6 +9,10 @@ app.use(express.json());
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 
+app.get('/', (req, res) => {
+    res.send('Bienvenido');
+});
+
 app.listen(port, () => {
     console.log(`La aplicación está escuchando en el puerto ${port}`);
 });
